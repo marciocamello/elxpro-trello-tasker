782654f3438de0a714bf2c331f4337acc85e0ed0
marciio camello <mac3designer@gmail.com>
2020-12-16 23:03:13 -0300

    Added trello api and implementing trello service to cards with live view

--- lib/trello_tasker_web/live/card_live.ex
+++ lib/trello_tasker_web/live/card_live.ex
@@ -2,10 +2,18 @@ defmodule TrelloTaskerWeb.CardLive do
   use TrelloTaskerWeb, :live_view
 
   alias Phoenix.View
+  alias TrelloTasker.Cards.Card
   alias TrelloTaskerWeb.CardView
+  alias TrelloTasker.Shared.Services.Trello
 
   @impl true
   def mount(_params, _session, socket) do
+
+    changeset = Card.changeset(%Card{})
+    card = Trello.get_card("jyliuSO2")
+
+    socket = socket
+    |> assign(cards: [card, card], changeset: changeset)
     {:ok, socket}
   end
 
